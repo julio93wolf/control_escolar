8ad10a20496c6c31ef0653036f1e32296fa6322b
Julio Rodriguez <juliovr_93@hotmail.com>
2018-04-02 23:08:51 -0500

    Periodos: Corrección de helper old
    Se corrigió la prioridadd del helper old en el modulo de periodos

--- resources/views/private/admin/escolares/periodos/forms/form.blade.php
+++ resources/views/private/admin/escolares/periodos/forms/form.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="input-field col s12">
 		<i class="material-icons prefix">account_circle</i>
-		<input id="periodo" name="periodo" type="text" class="validate" required="" aria-required="true" value="@if(isset($periodo)){{ $periodo->periodo }}@else{{ old('periodo') }}@endif">
+		<input id="periodo" name="periodo" type="text" class="validate" required="" aria-required="true" value="@if(old('periodo')){{ old('periodo') }}@elseif(isset($periodo)){{ $periodo->periodo }}@endif">
 		<label for="periodo" >Periodo</label>
 	</div>
 </div>
@@ -9,7 +9,7 @@
 <div class="row">
 	<div class="input-field col s12">
 		<i class="material-icons prefix">date_range</i>
-		<input id="anio" name="anio" type="number" class="validate" required="" aria-required="true" value="@if(isset($periodo)){{ $periodo->anio }}@else{{ old('anio') }}@endif">
+		<input id="anio" name="anio" type="number" class="validate" required="" aria-required="true" value="@if(old('anio')){{old('anio')}}@elseif(isset($periodo)){{ $periodo->anio }}@endif">
 		<label for="anio">Año</label>
 	</div>
 </div>
@@ -20,7 +20,7 @@
 		<input id="reconocimiento_oficial" name="reconocimiento_oficial" type="text" class="validate 
 			@if( $errors->has('reconocimiento_oficial')) 
 				invalid  
-			@endif" required="" aria-required="true" value="@if(isset($periodo)){{ $periodo->reconocimiento_oficial }}@else{{ old('reconocimiento_oficial') }}@endif">
+			@endif" required="" aria-required="true" value="@if(old('reconocimiento_oficial')){{old('reconocimiento_oficial')}}@elseif(isset($periodo)){{ $periodo->reconocimiento_oficial }}@endif">
 		<label for="reconocimiento_oficial" 
 			@if( $errors->has('reconocimiento_oficial')) 
 				class="active"
@@ -35,7 +35,7 @@
 		<input id="dges" name="dges" type="text" class="validate 
 			@if( $errors->has('dges')) 
 				invalid  
-			@endif" required="" aria-required="true" value="@if(isset($periodo)){{ $periodo->dges }}@else{{ old('dges') }}@endif">
+			@endif" required="" aria-required="true" value="@if(old('dges')){{old('dges')}}@elseif(isset($periodo)){{ $periodo->dges }}@endif">
 		<label for="dges"
 		@if( $errors->has('dges')) 
 			class="active"
@@ -48,7 +48,7 @@
 <div class="row">
 	<div class="input-field col s12">
 		<i class="material-icons prefix">date_range</i>
-		<input id="fecha_reconocimiento" name="fecha_reconocimiento" type="text" class="datepicker" required="" aria-required="true" value="@if(isset($periodo)){{ $periodo->fecha_reconocimiento }}@else{{ old('fecha_reconocimiento') }}@endif">
+		<input id="fecha_reconocimiento" name="fecha_reconocimiento" type="text" class="datepicker" required="" aria-required="true" value="@if(old('fecha_reconocimiento')){{old('fecha_reconocimiento')}}@elseif(isset($periodo)){{ $periodo->fecha_reconocimiento }}@endif">
 		<label for="fecha_reconocimiento">Fecha de reconocimiento</label>
 	</div>
 </div>
